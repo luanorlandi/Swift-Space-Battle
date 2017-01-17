a128d8840ff1353b2cc05e02a14ae737ac91ac30
Orlandi <orlandi.luan@gmail.com>
2017-01-17 16:50:21 -0200

    Fix Android bug in sprite sheet
    The bug was caused by wide rectangular tile, instead of square, causing memory allocation problems

--- src/effect/explosion.lua
+++ src/effect/explosion.lua
@@ -3,12 +3,12 @@ local expType2Size = Vector:new(250 * window.scale, 250 * window.scale)
 
 expType1 = MOAITileDeck2D:new()
 expType1:setTexture("texture/effect/expType1.png")
-expType1:setSize(64, 1)
+expType1:setSize(8, 8)
 expType1:setRect(-expType1Size.x, -expType1Size.y, expType1Size.x, expType1Size.y)
 
 expType2 = MOAITileDeck2D:new()
 expType2:setTexture("texture/effect/expType2.png")
-expType2:setSize(48, 1)
+expType2:setSize(7, 7)
 expType2:setRect(-expType2Size.x, -expType2Size.y, expType2Size.x, expType2Size.y)
 
 Explosion = {}
@@ -17,10 +17,14 @@ Explosion.__index = Explosion
 function Explosion:new(deck, pos, duration, frames)
 	local E = {}
 	setmetatable(E, Explosion)
+
+	E.remapper = MOAIDeckRemapper.new ()
+	E.remapper:reserve(frames)
 	
 	E.sprite = MOAIProp2D.new()
 	changePriority(E.sprite, "effect")
 	E.sprite:setDeck(deck)
+	E.sprite:setRemapper(E.remapper)
 	
 	E.pos = pos
 	
@@ -32,13 +36,14 @@ function Explosion:new(deck, pos, duration, frames)
 	
 	local t = 0.0
 	for i = 1, E.frames, 1 do
+		-- index, time in curve, value, mode
 		E.curve:setKey(i, t, i)
 		t = t + E.duration / E.frames
 	end
 	
 	E.anim = MOAIAnim:new()
 	E.anim:reserveLinks(1)
-	E.anim:setLink(1, E.curve, E.sprite, MOAIProp2D.ATTR_INDEX)
+	E.anim:setLink(1, E.curve, E.remapper, 1)
 	E.anim:setSpan(E.duration)
 	
 	E.complete = false
@@ -65,7 +70,7 @@ ExplosionType2.__index = ExplosionType2
 
 function ExplosionType2:new(pos, duration)
 	local E = {}
-	E = Explosion:new(expType2, pos, duration, 48)
+	E = Explosion:new(expType2, pos, duration, 49)
 	setmetatable(E, ExplosionType2)
 	
 	return E;
