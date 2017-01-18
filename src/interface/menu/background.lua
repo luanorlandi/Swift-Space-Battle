f3103364043f6ad79e3a15cd9b4a571ed6d4dab3
Orlandi <orlandi.luan@gmail.com>
2017-01-18 00:26:24 -0200

    Anim menu background, add jpg html config

--- src/interface/menu/background.lua
+++ src/interface/menu/background.lua
@@ -1,6 +1,10 @@
 local deck = MOAIGfxQuad2D.new()
-deck:setTexture("texture/background/menuBackground.png")
+deck:setTexture("texture/background/menuBackground.jpg")
 deck:setRect(-window.width/2, -window.height/2, window.width/2, window.height/2)
+
+local deckGlow = MOAIGfxQuad2D.new()
+deckGlow:setTexture("texture/background/menuBackgroundGlow.jpg")
+deckGlow:setRect(-window.width/2, -window.height/2, window.width/2, window.height/2)
 
 Background = {}
 Background.__index = Background
@@ -12,13 +16,33 @@ function Background:new()
 	B.sprite = MOAIProp2D.new()
 	changePriority(B.sprite, "interface")
 	B.sprite:setDeck(deck)
-	
 	B.sprite:setLoc(0, 0)
+
+	B.sprite:setBlendMode(MOAIProp.GL_SRC_ALPHA,
+		MOAIProp.GL_ONE_MINUS_SRC_ALPHA)
+
+	--B.sprite:setColor(0.05, 0.05, 0.05, 1)
+	--B.sprite:seekColor(1, 1, 1, 1, 1.0)
+	
+	B.spriteGlow = MOAIProp2D.new()
+	changePriority(B.spriteGlow, "interface")
+	B.spriteGlow:setDeck(deckGlow)
+	B.spriteGlow:setLoc(0, 0)
+
+	B.spriteGlow:setBlendMode(MOAIProp.GL_SRC_ALPHA,
+		MOAIProp.GL_ONE_MINUS_SRC_ALPHA)
+
+	B.spriteGlow:setColor(0.05, 0.05, 0.05, 1)
+	B.spriteGlow:seekColor(1, 1, 1, 1, 6.0)
+	B.spriteGlow:seekColor(1, 1, 1, 0, 10.0)
+
 	window.layer:insertProp(B.sprite)
+	window.layer:insertProp(B.spriteGlow)
 	
 	return B
 end
 
 function Background:clear()
 	window.layer:removeProp(self.sprite)
+	window.layer:removeProp(self.spriteGlow)
 end
