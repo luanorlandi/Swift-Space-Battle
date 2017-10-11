9384a7eb964ac57e89205859caf37e99f389f5ce
geritol <gergo.tolnai@gmail.com>
2017-10-11 18:22:37 +0200

    added Hungarian translation

---
+++ src/file/hu.lua
@@ -0,0 +1,23 @@
+language["hu"] = {
+    name = "HUNGARIAN",
+
+    menu = {
+        play = "JÁTÉK",
+        score = "LEGMAGASABB PONTSZÁM",
+        options = "BEÁLLÍTÁSOK",
+        about = "INFÓ",
+        quit = "KILÉPÉS",
+        back = "VISSZA",
+        resolution = "FELBONTÁS",
+        language = "NYELV",
+        restart = "ÚJRAINDÍTÁS (VÁLLTOZÁSOK ALLKALMAZÁSA)"
+    },
+
+    game = {
+        gameOver = "JÁTÉK VÉGE",
+        record = "ÚJ RECORD:",
+        again = "ÚJ JÁTÉK?",
+        yesAnswer = "IGEN",
+        noAnswer = "NEM"
+    }
+}
