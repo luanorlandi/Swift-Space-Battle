62e99c3e0a42983dfa23a52fb2686c0fccd7c88d
leogorman <leogorman95@gmail.come>
2017-10-11 11:05:09 +0100

    Translated game to french

---
+++ src/file/fr.lua
@@ -0,0 +1,23 @@
+language["fr"] = {
+    name = "FRENCH",
+
+    menu = {
+        play = "JOUER",
+        score = "LE SCORE LE PLUS ÉLEVÉ",
+        options = "OPTIONS",
+        about = "SUR",
+        quit = "QUITTER",
+        back = "ARRIÈRE",
+        resolution = "RÉSOLUTION",
+        language = "LA LANGUE",
+        restart = "REDÉMARRER POUR APPLIQUER DES CHANGEMENTS"
+    },
+
+    game = {
+        gameOver = "JEU TERMINÉ",
+        record = "NOUVEL ENREGISTREMENT:",
+        again = "REJOUER?",
+        yesAnswer = "OUI",
+        noAnswer = "NON"
+    }
+}
