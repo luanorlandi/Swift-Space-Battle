f247625e1ce79d424f6bdd27beb434439736eceb
arrowfan <simpen_10@msn.com>
2017-10-12 16:40:02 +0200

    Added swedish language - native speaker

---
+++ src/file/sv.lua
@@ -0,0 +1,23 @@
+language["sv"] = {
+    name = "SVENSKA",
+
+    menu = {
+        play = "SPELA",
+        score = "REKORD",
+        options = "ALTERNATIV",
+        about = "OM",
+        quit = "AVSLUTA",
+        back = "TILLBAKA",
+        resolution = "UPPLÖSNING",
+        language = "SPRÅK",
+        restart = "STARTA OM FÖR ATT ÄNDRINGAR SKALL APPLICERAS"
+    },
+
+    game = {
+        gameOver = "SPELET ÖVER",
+        record = "NYTT REKORD:",
+        again = "SPELA IGEN?",
+        yesAnswer = "JA",
+        noAnswer = "NEJ"
+    }
+}
