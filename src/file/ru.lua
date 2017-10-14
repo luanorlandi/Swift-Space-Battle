2efa1209d4c6f50b6b196a42ef88c53e6b90c6de
eugeneloza <Loza@i.ua>
2017-10-14 21:53:38 +0300

    fix linebreaks for Russian translation

--- src/file/ru.lua
+++ src/file/ru.lua
@@ -1,1 +1,23 @@
-language["ru"] = {    name = "РУССКИЙ",    menu = {        play = "ИГРАТЬ",        score = "РЕКОРДЫ",        options = "НАСТРОЙКИ",        about = "ПРО ИГРУ",        quit = "ВЫХОД",        back = "НАЗАД",        resolution = "РАЗРЕШЕНИЕ",        language = "ЯЗЫК",        restart = "ПЕРЕЗАГРУЗИТЕ ЧТОБЫ ИЗМЕНЕНИЯ ВСТУПИЛИ В СИЛУ"    },    game = {        gameOver = "ИГРА ОКОНЧЕНА",        record = "НОВЫЙ РЕКОРД:",        again = "СЫГРАЕМ ЕЩЁ РАЗ?",        yesAnswer = "ДА",        noAnswer = "НЕТ"    }}
+language["ru"] = {
+    name = "РУССКИЙ",
+
+    menu = {
+        play = "ИГРАТЬ",
+        score = "РЕКОРДЫ",
+        options = "НАСТРОЙКИ",
+        about = "ПРО ИГРУ",
+        quit = "ВЫХОД",
+        back = "НАЗАД",
+        resolution = "РАЗРЕШЕНИЕ",
+        language = "ЯЗЫК",
+        restart = "ПЕРЕЗАГРУЗИТЕ ИГРУ ЧТОБЫ ИЗМЕНЕНИЯ ВСТУПИЛИ В СИЛУ"
+    },
+
+    game = {
+        gameOver = "ИГРА ОКОНЧЕНА",
+        record = "НОВЫЙ РЕКОРД:",
+        again = "СЫГРАЕМ ЕЩЁ РАЗ?",
+        yesAnswer = "ДА",
+        noAnswer = "НЕТ"
+    }
+}
