b0a16531c433656d62f53fef9c183d0e8d013f6e
Chriztian Steinmeier <chriztian@steinmeier.dk>
2017-10-15 17:08:57 +0200

    Add da.lua and translate keys

---
+++ src/file/da.lua
@@ -0,0 +1,23 @@
+language["da"] = {
+    name = "DANSK",
+
+    menu = {
+        play = "START",
+        score = "REKORD",
+        options = "VALG",
+        about = "OM",
+        quit = "AFSLUT",
+        back = "TILBAGE",
+        resolution = "OPLØSNING",
+        language = "SPROG",
+        restart = "GENSTART FOR BRUGE ÆNDRINGER"
+    },
+
+    game = {
+        gameOver = "SLUT",
+        record = "NY REKORD:",
+        again = "SPIL IGEN?",
+        yesAnswer = "JA",
+        noAnswer = "NEJ"
+    }
+}
