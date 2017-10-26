8cab30c2f0351c0a03fc9d885438b4fed151f4d2
Lukáš Hruška <lukyhruska96@gmail.com>
2017-10-26 18:00:13 +0200

    Added Czech language translation

---
+++ src/file/cs.lua
@@ -0,0 +1,23 @@
+language["cs"] = {
+    name = "ČEŠTINA",
+
+    menu = {
+        play = "HRAJ",
+        score = "NEJVYŠŠÍ SKÓRE",
+        options = "NASTAVENÍ",
+        about = "O HŘE",
+        quit = "ODEJÍT",
+        back = "ZPĚT",
+        resolution = "ROZLIŠENÍ",
+        language = "JAZYK",
+        restart = "RESTARTUJTE HRU PRO NASTAVENÍ ZMĚN"
+    },
+
+    game = {
+        gameOver = "KONEC HRY",
+        record = "NOVÝ REKORD:",
+        again = "HRÁT ZNOVU?",
+        yesAnswer = "ANO",
+        noAnswer = "NE"
+    }
+}
